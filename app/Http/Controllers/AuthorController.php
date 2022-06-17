963bcfed8e68d40cd423062a037a283c2a14dfad
PW kazu <komura_jutasrou@icloud.com>
2022-06-17 16:11:01 +0900

    first-commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illiminate\Support\Facades\DB;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $items = DB::select('select * from authors');
+        return view('index', ['items'=>$items]);
+    }
+    public function add()
+    {
+        return view('add');
+    }
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'age' => $request->age,
+            'nationality' => $request->nationality,
+        ];
+        DB::insert('insert into authors (name, age, nationality) values (:name, :age, :nationality)', $param);
+        return redirect('/');
+    }
+}
+
+?>
