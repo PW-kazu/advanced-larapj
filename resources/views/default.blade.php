4f206bd2f9324e97750348e819936de8761f881b
PW kazu <komura_jutasrou@icloud.com>
2022-10-19 07:48:13 +0900

    修正

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>`yield('title')</title>
+  <title>@yield('title')</title>
   <style>
   body {
     font-size:16px;
@@ -25,9 +25,8 @@
 </head>
 <body>
   <h1>@yield('title')</h1>
-  <div class='content'>
-    @yield('content')
+  <div class="content">
+  @yield('content')
   </div>
-  
 </body>
 </html>
